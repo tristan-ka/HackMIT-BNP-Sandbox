289ccd4bfc3000742544eba3a67e5751836f7a0c
tristan-ka <tristan-ka>
2018-09-14 23:10:51 -0400

    adding python sdk requests

---
+++ props/.ipynb_checkpoints/default-checkpoint.py
@@ -0,0 +1,8 @@
+OUR_BANK     = ''
+
+USERNAME     = 'your username'
+PASSWORD     = 'your password'
+CONSUMER_KEY = 'your API key'
+
+BASE_URL        = 'https://bnpparibas-irb.openbankproject.com'
+API_VERSION     = 'v3.1.0'
